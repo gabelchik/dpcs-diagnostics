62a607b77fe07f664a2f2aa8234128e8b3d4a5bd
Leonid Kalintsev <lensuperhero@gmail.com>
2026-07-19 12:07:33 +0300

    Delete dpcs-diagnostics directory

--- dpcs-diagnostics/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-from .resource_module import ResourceModule
-from .integrity_module import IntegrityModule
-from .enviroment_module import EnvironmentModule
-
-MODULE_REGISTRY = {
-    "resources": ResourceModule,
-    "integrity": IntegrityModule,
-    "environment": EnvironmentModule
-}
--- dpcs-diagnostics/responders/__init__.py
+++
@@ -1,5 +0,0 @@
-from .file_responder import FileResponder
-
-RESPONDER_REGISTRY = {
-    "file_responder": FileResponder,
-}
